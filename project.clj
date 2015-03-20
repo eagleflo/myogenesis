fd7b8e2322b3d3d3ff21c05727e5a7d43e20feda
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2015-03-20 13:26:40 +0200

    First version of Myogenesis

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject myogenesis "0.1.0-SNAPSHOT"
+  :description "Myogenesis, a fitness tracker"
+  :url "https://eagleflow.fi/myogenesis/"
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.clojure/clojurescript "0.0-3058"]
+                 [reagent "0.5.0"]
+                 [figwheel "0.2.5"]]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-figwheel "0.2.5"]]
+  :source-paths ["src/clj"]
+  :cljsbuild {:builds [{:source-paths ["src/myogenesis"]
+                        :compiler {:main myogenesis.core
+                                   :output-to "out/myogenesis.js"
+                                   :optimizations :none}}]})
---
+++ src/myogenesis/core.cljs
@@ -0,0 +1,74 @@
+(ns myogenesis.core
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(enable-console-print!)
+(println "Hello, myogenesis!")
+
+;; Workout is a map of exercises to a vector of sets.
+;; Starting Strength workout A:
+(defonce workout (atom {"Squat" [{:weight 20 :reps 5}
+                                 {:weight 20 :reps 5}
+                                 {:weight 40 :reps 5}
+                                 {:weight 60 :reps 3}
+                                 {:weight 80 :reps 2}
+                                 {:weight 100 :reps 5}
+                                 {:weight 100 :reps 5}
+                                 {:weight 100 :reps 5}]
+                        "Bench Press" [{:weight 20 :reps 5}
+                                       {:weight 20 :reps 5}
+                                       {:weight 40 :reps 5}
+                                       {:weight 55 :reps 3}
+                                       {:weight 70 :reps 2}
+                                       {:weight 80 :reps 5}
+                                       {:weight 80 :reps 5}
+                                       {:weight 80 :reps 5}]
+                        "Deadlift" [{:weight 50 :reps 5}
+                                    {:weight 50 :reps 5}
+                                    {:weight 75 :reps 3}
+                                    {:weight 100 :reps 2}
+                                    {:weight 120 :reps 5}]}))
+
+(defonce exercises ["Squat" "Press" "Deadlift" "Bench Press" "Power Clean"])
+(defonce current-exercise (atom "Squat"))
+(defonce weight {:name "Weight" :data (atom nil)})
+(defonce reps {:name "Reps" :data (atom nil)})
+
+(defn select-exercise [exercise]
+  [:div.button {:on-click #(reset! current-exercise exercise)} exercise])
+
+(defn input-row [{:keys [name data]}]
+  [:div
+   [:label {:for name} name]
+   [:input {:id name
+            :type "number"
+            :on-change #(reset! data (-> % .-target .-value))}]])
+
+(defn add-set! [exercise weight reps]
+  (swap! workout #(update-in % [exercise] conj {:weight @weight :reps @reps})))
+
+(defn set-form [exercise]
+  [:div#add-set
+   [input-row weight]
+   [input-row reps]
+   [:input {:type "button" :value "Enter"
+            :on-click #(add-set! exercise (:data weight) (:data reps))}]])
+
+(defn setlist [move sets]
+  [:div.setlist
+   [:p move]
+   [:ol
+    (for [[idx {:keys [weight reps]}] (map-indexed vector sets)]
+      ^{:key (str move idx)} [:li (str weight " kg x " reps)])]])
+
+(defn app []
+  [:div
+   [:h1 "Myogenesis"]
+   [:div#exercises
+    (for [exercise exercises]
+      ^{:key (str "add-" exercise)} [select-exercise exercise])]
+   [set-form @current-exercise]
+   [:div#setlists
+    (for [[move sets] @workout]
+      ^{:key move} [setlist move sets])]])
+
+(reagent/render [app] (.-body js/document))
