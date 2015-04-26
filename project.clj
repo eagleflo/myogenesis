bfaa15e51528a8452a75a5a9162372e7597b65b3
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2015-04-26 23:04:41 +0300

    Update figwheel

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/clojurescript "0.0-3211"]
                  [reagent "0.5.0"]
-                 [figwheel "0.2.5"]]
+                 [figwheel "0.2.7"]]
   :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-figwheel "0.2.5"]]
+            [lein-figwheel "0.2.7"]]
   :source-paths ["src/clj"]
   :cljsbuild {:builds [{:source-paths ["src/myogenesis"]
                         :compiler {:main myogenesis.core
